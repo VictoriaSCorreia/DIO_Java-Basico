828e53d17d804b41d73d0442e3282aad2692db3c
VictoriaSCorreia <victoriacorreiavic@gmail.com>
2024-06-01 18:00:23 -0300

    Update

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Número da conta: ");
+        numero = input.nextInt();
+        input.nextLine();
+        System.out.print("Nome da agência: ");
+        agencia = input.nextLine();
+        System.out.print("Nome do cliente: ");
+        nomeCliente = input.nextLine();
+        System.out.print("Saldo: ");
+        saldo = input.nextDouble();
+
+        System.out.printf("Olá, %s! Obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+
+        input.close();
     }
 }
+
